13a4a0e1015059b4b5e0db7cac8ad543a6eb1aba
Adam Victor <adamvictor@roboretail.us>
2021-07-27 21:54:35 -0400

    Added routing, updated BS to BS 5.

--- src/app/dashboard1/dashboard1-routing.module.ts
+++ src/app/dashboard1/dashboard1-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Dashboard1NavComponent } from './dashboard1-nav/dashboard1-nav.component';
+import { Dashboard1Component } from './dashboard1/dashboard1.component';
 
 const routes: Routes = [
-
+  { path: 'dashboard1', component: Dashboard1Component,
+    children: [
+      { path: 'dashboard1-nav', component: Dashboard1NavComponent}
+    ]
+  }
 ];
 @NgModule({
   imports: [
